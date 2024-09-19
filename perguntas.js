2628945cdfe076ac41ba525dec6fa0d824dc6d0b
brunaslb <125621275+brunaslb@users.noreply.github.com>
2024-09-19 11:56:30 +0000

    n

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Física'
+    'Como tranformar Km/h para M/s?',
+    'K/h dividido por 3,6 M/s'
+)
+
+criaCartao(
+    'Física'
+    'Como calcular ∆s?',
+    '∆s= V*∆t'
+)
+
+criaCartao(
+    'Física'
+    'Como calcular ∆t?',
+    '∆t= ∆5/V'
+)
+
+criaCartao(
+    'Física'
+    'Qual a fórmula da aceleração média (Am)?';
+    '∆V/∆E'
+)
