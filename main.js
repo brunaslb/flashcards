9330f60f73a2f1d906d4ae51e7d5b833bac2f535
brunaslb <125621275+brunaslb@users.noreply.github.com>
2024-09-23 08:54:24 -0300

    .

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 function criaCartao (categoria, pergunta, resposta){
-    
-}
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartaoinnerHTML = `
+
+<div class="conteudo-cartao">
+<h3>${categoria}</h3>
+<div class="pergunta-cartao">
+<p>${pergunta}</p>
+</div>
+<div class="resposta-cartao">
+    <p>${resposta}</p>
+
+</div>
+</div>
+`
+container.appendChild(cartao);
+
+    }
--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,99 @@ criaCartao(
     'Qual a fórmula da aceleração média (Am)?',
     '∆V/∆E'
 )
+
+criaCartao(
+    'Física',
+    'Qual a fórmula da velocidade média (Vm)?',
+    '∆S/∆T'
+)
+
+criaCartao(
+    'Física',
+    'Oque é o ∆ na física?',
+    '∆ = delta, ∆ = Valor Final - Valor Inicial'
+)
+
+criaCartao(
+    'Física',
+    'Qual a lei de Hokkie (força elástica)?',
+    'Força elástica = -K.X'
+)
+
+criaCartao(
+    'Física',
+    'Qual a fórmula de Torricelli?',
+    'VF2 = Vi2+2.a.∆x'
+)
+
+criaCartao(
+    'História',
+    'O que é latifundio?',
+    'Grandes proporções de terras'
+)
+
+criaCartao(
+    'Matemática',
+    'Fórmula do Teorema de Pitagoras?',
+    'Hip² = Cat1² + Cat2²'
+)
+
+criaCartao(
+    'História',
+    'O que é Monocultura?',
+    'É quando as terras produzem uma cultura apenas'
+)
+
+criaCartao(
+    'História',
+    'O que é policultura?',
+    'é quando produz mais de um tipo de cultura'
+)
+
+criaCartao(
+    'Matemática',
+    'Formúla da área de um triangulo?',
+    'B.H/2'
+)
+
+criaCartao(
+    'Matemática',
+    'Fórmula para sioma dos angulos internos?',
+    'Si = 180 (N-2)'
+)
+
+criaCartao(
+    'Física',
+    'Fórmula do MRU?',
+    'S= S0 + V.t'
+)
+
+criaCartao(
+    'Física',
+    'Fórmula do MRUV?',
+    'S0 + V0.T+0,5.a.t²'
+)
+
+criaCartao(
+    'Física',
+    'Formula da eletricidade?',
+    'U = R.I'
+)
+
+criaCartao(
+    'Física',
+    'Fórmula do Trabalho?',
+    'Trab. = F.D'
+)
+
+criaCartao(
+    'Matemática',
+    'Formúla da área do circulo?',
+    'Pi.R²'
+)
+
+criaCartao(
+    'Matemática',
+    'Formúla do comprimento da circunferência?',
+    '2.Pi.R'
+)
